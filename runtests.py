8aa903b59494485114de8ec75d59674332549e0d
Emil Stenström <em@kth.se>
2020-03-07 15:53:29 +0100

    Add tests for passing in and receiving datetime objects.

--- runtests.py
+++ runtests.py
@@ -1,11 +1,26 @@
 import pytz
 import unittest
+from datetime import datetime
 from emojizones import convert
 from emojizones.lookup import EMOJI_TO_TIMEZONE
 
 class ConvertTest(unittest.TestCase):
 
-    def test_convert_basic(self):
+    def test_convert_datetime(self):
+        self.assertEqual(
+            convert(datetime(2020, 3, 7, 0, 0, 0), "🗻", "🗽"),
+            pytz.timezone('America/New_York').localize(datetime(2020, 3, 6, 10, 0, 0)),
+        )
+        self.assertEqual(
+            convert(datetime(2020, 3, 7, 0, 0), "🗻", "🗽", as_string=True),
+            "2020-03-06 10:00:00",
+        )
+
+    def test_convert_string(self):
+        self.assertEqual(
+            convert("2020-03-07 00:00:00", "🗻", "🗽"),
+            pytz.timezone('America/New_York').localize(datetime(2020, 3, 6, 10, 0, 0)),
+        )
         self.assertEqual(
             convert("2020-03-07 00:00:00", "🗻", "🗽", as_string=True),
             "2020-03-06 10:00:00",
